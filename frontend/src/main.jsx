314fccae1ea60a79253f0e65d11e4ebe58d24b8f
Sujana Kasireddy <sujanakasireddy2@gmail.com>
2026-05-02 11:55:53 +0530

    fix App.jsx case issue

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
+import App from './app.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import './index.css'
